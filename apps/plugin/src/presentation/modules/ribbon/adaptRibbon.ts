cfa3ea06cee80a6cb135a68e269b34f18267a94f
Mimikkk <zdancewicz.daniel@gmail.com>
2025-05-02 15:03:47 +0200

    add effect

--- apps/plugin/src/presentation/modules/ribbon/adaptRibbon.ts
+++ apps/plugin/src/presentation/modules/ribbon/adaptRibbon.ts
@@ -1,11 +1,13 @@
 import { sync } from "@plugin/domain/signals/isSyncing.ts";
 import { createAdapter } from "@plugin/infrastructure/createAdapter.ts";
+import { createRibbonAdapterEffect } from "@plugin/presentation/modules/ribbon/effects/createRibbonAdapterEffect.ts";
 import { onCleanup } from "solid-js";
 import { render } from "solid-js/web";
 import { RibbonBar } from "./components/RibbonBar.tsx";
 
 export const adaptRibbon = createAdapter((plugin) => {
   const ribbon = plugin.addRibbonIcon("cloud", "Synchronize", sync);
+  createRibbonAdapterEffect(ribbon);
 
   const root = ribbon.createDiv("div");
   onCleanup(() => root.remove());
