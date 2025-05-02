dd40aec6714b0127dbf1403b934360fe2b817994
Mimikkk <zdancewicz.daniel@gmail.com>
2025-05-02 23:41:57 +0200

    labels

--- apps/server/src/modules/health/presentation/messaging/http/responses/HealthResponse.ts
+++ apps/server/src/modules/health/presentation/messaging/http/responses/HealthResponse.ts
@@ -2,9 +2,9 @@ import { HttpJsonResponse } from "@server/presentation/messaging/http/responses/
 
 export namespace HealthResponse {
   export const [Ok, ok] = HttpJsonResponse.custom({
-    content: () => ({ status: "OK", message: "The server is running" }),
+    content: () => ({ status: "OK", message: "The service is running." }),
     description: "The health of the server",
-    example: { status: "OK", message: "The server is running" },
+    example: { status: "OK", message: "The service is running." },
     name: "Health",
     schema: { type: "object", properties: { status: { type: "string" }, message: { type: "string" } } },
     status: 200,
