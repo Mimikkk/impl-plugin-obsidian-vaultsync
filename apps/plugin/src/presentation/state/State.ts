9e00d9bc6414982bde2084749d4c37ee7a9349bd
Mimikkk <zdancewicz.daniel@gmail.com>
2025-05-11 00:53:48 +0200

    state

--- apps/plugin/src/presentation/state/State.ts
+++ apps/plugin/src/presentation/state/State.ts
@@ -1,7 +1,7 @@
 import { LocalStorageParameters } from "@plugin/infrastructure/storage/parameters/LocalStorageParameters.ts";
 import { LocalStorage } from "../../infrastructure/storage/LocalStorage.ts";
 
-export namespace Memory {
+export namespace State {
   export const lastSeenEventId = LocalStorage.create<number>(
     "lastSeenEventId",
     LocalStorageParameters.number(0),
