89e539a624de3592d5cdb17b8367f3dc95641d99
Mimikkk <zdancewicz.daniel@gmail.com>
2025-05-12 00:03:57 +0200

    rename

--- libs/shared/src/mod.ts
+++ libs/shared/src/mod.ts
@@ -1,7 +1,7 @@
 export { noop, today } from "./consts.ts";
 export { HttpMethod } from "./enums/HttpMethod.ts";
 export type { Awaitable, Every, KeyBy, Merge, Nil, Some } from "./types/common.ts";
-export type { TypeKey } from "./types/typedKey.ts";
+export type { TypeKey } from "./types/typeKey.ts";
 export { type DateInit, DateTimeNs, DateTimeStr } from "./utils/DateTimeStr.ts";
 export { Str } from "./utils/strings.ts";
 export { TimeMs } from "./utils/TimeMs.ts";
